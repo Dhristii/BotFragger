204df55568b3f32b34ecbec63bfb01860776bf81
shouko-sy <yudisgrd1@gmail.com>
2020-11-02 23:13:57 +0700

    minor

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 let fs = require('fs');
 const { deflate } = require('zlib');
-
 const prefix = '#';
-
 const version = '1.0.2'
+
+
+
+
 
 bot.on('ready', function(){
     console.log('pantekkk');
@@ -19,6 +21,26 @@ bot.on('message', function(message){
     let lineReader = require('readline').createInterface({
         input: fs.createReadStream(__dirname + '/botfrager.txt')
     });
+
+    function print(array){
+        let print = {};
+        print[0] = array[0];
+        print[1] = 'Dd ' + array[1];
+        print[2] = 'Shouko ' + array[2];
+        print[3] = 'Datonkun ' + array[3];
+        print[4] = 'Kuhaku Desu ' + array[4];
+        print[5] = '';
+        print[6] = array[6];
+        print[7] = 'Dd ' + array[7];
+        print[8] = 'Shouko ' + array[8];
+        print[9] = 'Datonkun ' + array[9];
+        print[10] = 'Kuhaku Desu ' + array[10];
+        print[11] = '';
+        print[12] = 'Match : ' + array[12];
+
+        let temp = Object.keys(print).map((key) => [print[key]]);
+        message.channel.send(temp);
+    }
 
     switch(chat[0]){
         case 'update' : 
@@ -40,23 +62,7 @@ bot.on('message', function(message){
                     array[1] = parseInt(array[1]) + 1;
                     array[12] = parseInt(array[12]) + 1;
 
-                    let print = {};
-                    print[0] = array[0];
-                    print[1] = 'Dd ' + array[1];
-                    print[2] = 'Shouko ' + array[2];
-                    print[3] = 'Datonkun ' + array[3];
-                    print[4] = 'Kuhaku Desu ' + array[4];
-                    print[5] = '';
-                    print[6] = array[6];
-                    print[7] = 'Dd ' + array[7];
-                    print[8] = 'Shouko ' + array[8];
-                    print[9] = 'Datonkun ' + array[9];
-                    print[10] = 'Kuhaku Desu ' + array[10];
-                    print[11] = '';
-                    print[12] = 'Match : ' + array[12];
-
-                    let temp = Object.keys(print).map((key) => [print[key]]);
-                    message.channel.send(temp);
+                    print(array);
 
                     let toWrite = array;
                     fs.writeFile('botfrager.txt', toWrite, function(err){
@@ -79,23 +85,7 @@ bot.on('message', function(message){
                     array[2] = parseInt(array[2]) + 1;
                     array[12] = parseInt(array[12]) + 1;
 
-                    let print = {};
-                    print[0] = array[0];
-                    print[1] = 'Dd ' + array[1];
-                    print[2] = 'Shouko ' + array[2];
-                    print[3] = 'Datonkun ' + array[3];
-                    print[4] = 'Kuhaku Desu ' + array[4];
-                    print[5] = '';
-                    print[6] = array[6];
-                    print[7] = 'Dd ' + array[7];
-                    print[8] = 'Shouko ' + array[8];
-                    print[9] = 'Datonkun ' + array[9];
-                    print[10] = 'Kuhaku Desu ' + array[10];
-                    print[11] = '';
-                    print[12] = 'Match : ' + array[12];
-
-                    let temp = Object.keys(print).map((key) => [print[key]]);
-                    message.channel.send(temp);
+                    print(array);
 
                     let toWrite = array;
                     fs.writeFile('botfrager.txt', toWrite, function(err){
@@ -155,23 +145,7 @@ bot.on('message', function(message){
                     array[4] = parseInt(array[4]) + 1;
                     array[12] = parseInt(array[12]) + 1;
 
-                    let print = {};
-                    print[0] = array[0];
-                    print[1] = 'Dd ' + array[1];
-                    print[2] = 'Shouko ' + array[2];
-                    print[3] = 'Datonkun ' + array[3];
-                    print[4] = 'Kuhaku Desu ' + array[4];
-                    print[5] = '';
-                    print[6] = array[6];
-                    print[7] = 'Dd ' + array[7];
-                    print[8] = 'Shouko ' + array[8];
-                    print[9] = 'Datonkun ' + array[9];
-                    print[10] = 'Kuhaku Desu ' + array[10];
-                    print[11] = '';
-                    print[12] = 'Match : ' + array[12];
-
-                    let temp = Object.keys(print).map((key) => [print[key]]);
-                    message.channel.send(temp);
+                    print(array);
 
                     let toWrite = array;
                     fs.writeFile('botfrager.txt', toWrite, function(err){
@@ -195,24 +169,7 @@ bot.on('message', function(message){
             lineReader.on('line', function (line) {
                 let array = line.split(',');
                 
-                let print = {};
-                print[0] = array[0];
-                print[1] = 'Dd ' + array[1];
-                print[2] = 'Shouko ' + array[2];
-                print[3] = 'Datonkun ' + array[3];
-                print[4] = 'Kuhaku Desu ' + array[4];
-                print[5] = '';
-                print[6] = array[6];
-                print[7] = 'Dd ' + array[7];
-                print[8] = 'Shouko ' + array[8];
-                print[9] = 'Datonkun ' + array[9];
-                print[10] = 'Kuhaku Desu ' + array[10];
-                print[11] = '';
-                print[12] = 'Match : ' + array[12];
-
-                let temp = Object.keys(print).map((key) => [print[key]]);
-                message.channel.send(temp);
-                
+                print(array);
             });
             break;
 
@@ -241,23 +198,7 @@ bot.on('message', function(message){
                         message.channel.send('Match tidak di update!');
                     }
 
-                    let print = {};
-                    print[0] = array[0];
-                    print[1] = 'Dd ' + array[1];
-                    print[2] = 'Shouko ' + array[2];
-                    print[3] = 'Datonkun ' + array[3];
-                    print[4] = 'Kuhaku Desu ' + array[4];
-                    print[5] = '';
-                    print[6] = array[6];
-                    print[7] = 'Dd ' + array[7];
-                    print[8] = 'Shouko ' + array[8];
-                    print[9] = 'Datonkun ' + array[9];
-                    print[10] = 'Kuhaku Desu ' + array[10];
-                    print[11] = '';
-                    print[12] = 'Match : ' + array[12];
-
-                    let temp = Object.keys(print).map((key) => [print[key]]);
-                    message.channel.send(temp);
+                    print(array);
 
                     let toWrite = array;
                     fs.writeFile('botfrager.txt', toWrite, function(err){
@@ -288,23 +229,7 @@ bot.on('message', function(message){
                         message.channel.send('Match tidak di update!');
                     }
 
-                    let print = {};
-                    print[0] = array[0];
-                    print[1] = 'Dd ' + array[1];
-                    print[2] = 'Shouko ' + array[2];
-                    print[3] = 'Datonkun ' + array[3];
-                    print[4] = 'Kuhaku Desu ' + array[4];
-                    print[5] = '';
-                    print[6] = array[6];
-                    print[7] = 'Dd ' + array[7];
-                    print[8] = 'Shouko ' + array[8];
-                    print[9] = 'Datonkun ' + array[9];
-                    print[10] = 'Kuhaku Desu ' + array[10];
-                    print[11] = '';
-                    print[12] = 'Match : ' + array[12];
-
-                    let temp = Object.keys(print).map((key) => [print[key]]);
-                    message.channel.send(temp);
+                    print(array);
 
                     let toWrite = array;
                     fs.writeFile('botfrager.txt', toWrite, function(err){
@@ -335,23 +260,7 @@ bot.on('message', function(message){
                         message.channel.send('Match tidak di update!');
                     }
 
-                    let print = {};
-                    print[0] = array[0];
-                    print[1] = 'Dd ' + array[1];
-                    print[2] = 'Shouko ' + array[2];
-                    print[3] = 'Datonkun ' + array[3];
-                    print[4] = 'Kuhaku Desu ' + array[4];
-                    print[5] = '';
-                    print[6] = array[6];
-                    print[7] = 'Dd ' + array[7];
-                    print[8] = 'Shouko ' + array[8];
-                    print[9] = 'Datonkun ' + array[9];
-                    print[10] = 'Kuhaku Desu ' + array[10];
-                    print[11] = '';
-                    print[12] = 'Match : ' + array[12];
-
-                    let temp = Object.keys(print).map((key) => [print[key]]);
-                    message.channel.send(temp);
+                    print(array);
 
                     let toWrite = array;
                     fs.writeFile('botfrager.txt', toWrite, function(err){
@@ -382,23 +291,7 @@ bot.on('message', function(message){
                         message.channel.send('Match tidak di update!');
                     }
 
-                    let print = {};
-                    print[0] = array[0];
-                    print[1] = 'Dd ' + array[1];
-                    print[2] = 'Shouko ' + array[2];
-                    print[3] = 'Datonkun ' + array[3];
-                    print[4] = 'Kuhaku Desu ' + array[4];
-                    print[5] = '';
-                    print[6] = array[6];
-                    print[7] = 'Dd ' + array[7];
-                    print[8] = 'Shouko ' + array[8];
-                    print[9] = 'Datonkun ' + array[9];
-                    print[10] = 'Kuhaku Desu ' + array[10];
-                    print[11] = '';
-                    print[12] = 'Match : ' + array[12];
-
-                    let temp = Object.keys(print).map((key) => [print[key]]);
-                    message.channel.send(temp);
+                    print(array);
 
                     let toWrite = array;
                     fs.writeFile('botfrager.txt', toWrite, function(err){
